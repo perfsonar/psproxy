f6b2a2da389354e0c729e4ce973c0faeee0829d1
Ljubomir Hrboka <ljubomir@carnet.hr>
2020-04-30 13:45:58 +0200

    Removed flask_cors and added custom function

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-origin = app.config['APPURL']
-cors = CORS(app, resources={r"/api/*": {"origins": origin}})
-
 from app import routes
-
--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_in_production'
-    CORS_HEADERS = 'Content-Type'
     MADDASH = 'https://pmp-central.geant.org/maddash/rows'
     APPURL = 'http://localhost:3000'
