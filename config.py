9bffa981bbe0075a05214d74cc8ef01bfe192e6a
Ljubomir Hrboka <ljubomir@carnet.hr>
2020-05-21 11:42:37 +0200

    Removed config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_in_production'
-    MADDASH = 'https://pmp-central.geant.org/maddash/rows'
-    CORS_HEADERS = 'Content-Type'
