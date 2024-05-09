3f5d5f14d80f2430d6a0a760f0c426e6b9a2ea01
“ljubomir” <ljubomir@carnet.hr>
2024-05-09 17:12:02 +0200

    MADDAH changed with PSCONFIG

--- example_config.py
+++ example_config.py
@@ -4,5 +4,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_in_production'
-    MADDASH = 'https://pmp-central.geant.org/maddash/rows'
+    PSCONFIG = 'https://pmp-central.geant.org/pscfg-psmall.json'
     CORS_HEADERS = 'Content-Type'
