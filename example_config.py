23b6cf2f85bd4695b7301592598ec478d2fd8a02
“ljubomir” <ljubomir@carnet.hr>
2025-05-27 10:31:10 +0200

    Updated PMP pscfg-pmp.json file

--- example_config.py
+++ example_config.py
@@ -4,5 +4,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_in_production'
-    PSCONFIG = 'https://pmp-central.geant.org/pscfg-psmall.json'
+    PSCONFIG = 'https://pmp-archive.geant.org/psconfig/pscfg-pmp.json'
     CORS_HEADERS = 'Content-Type'
